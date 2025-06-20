5a0d66eaa21d089b5dda1178b54d20a09157bd0d
wk4t96 <120920127+wk4t96@users.noreply.github.com>
2025-06-21 04:07:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ outputBucketName = "s3-yjche-output"
 session = boto3.Session(
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION")
+    region_name=os.getenv("AWS_DEFAULT_REGION")
 )
 sqs = session.client("sqs")
 s3 = session.client("s3")
